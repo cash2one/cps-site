a26bcce0a09325c47e314040d051fda9dcbe0935
Maxis <maxis1718@gmail.com>
2014-09-10 00:13:11 +0800

    Add js-version language support

--- static/js/util.js
+++ static/js/util.js
@@ -1,14 +1,16 @@
 
+var support = ["zh-tw", "en"];
 
 $(window).on('hashchange', function(e){
 
 	var hash = location.hash;
 	var lang = hash.replace( /^#/, '' );
 
-	console.log('found'+lang)
+	// console.log('current language:', lang)
 
 	if(lang)
 	{
+		// console.log("change to", lang)
 		changeLanguage(lang);
 	}else
 	{
@@ -18,8 +20,29 @@ $(window).on('hashchange', function(e){
 
 function changeLanguage(lang)
 {
-	$('.text').addClass('hide');
-	$('.text[lang='+lang+']').removeClass('hide');
+	// find all tags with attribute "lang"
+	$.each( $('*[lang]'), function(i, this_obj){
+
+		// ignore <HTML> and <BODY> tags
+		if ( this_obj.tagName.toUpperCase() == "HTML"  || this_obj.tagName.toUpperCase() == "BODY")	{
+			return true;
+		}
+
+		// get the lang of this obj
+		var this_lang = $(this_obj).attr("lang");
+
+		if ( this_lang != lang )
+		{
+			if ( !$(this_obj).hasClass("hide") )
+			{
+				$(this_obj).addClass("hide");	
+			}			
+		}
+		else
+		{
+			$(this_obj).removeClass("hide");
+		}
+	})
 }
 
 
